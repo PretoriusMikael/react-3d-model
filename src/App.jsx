441f5fbab7c3b3c802233b0660bb7880ebaeeecd
Mikael <mikaelpretorius@gmail.com>
2025-04-04 08:46:37 +0200

    made app

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { Suspense } from 'react'
+import './App.css'
+import Footer from './Footer'
+import Header from './Header'
+
+import { Canvas } from '@react-three/fiber'
+import { Environment, OrbitControls, ContactShadows } from '@react-three/drei'
+import Earth from '../public/Earth'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Canvas>
+        <ambientLight />
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <Earth />
+        </Suspense>
+        <Environment preset='sunset'/>
+        <ContactShadows position={[0, -2.5, 0]} opacity={0.5} scale={50} blur={1} far={10} resolution={256} color="#000000"/>
+      </Canvas>
+      <div className='container'>
+        <h1>Earth</h1>
+        <p>Earth is the third planet from the Sun and the only astronomical object known to harbor life. This is enabled by Earth being an ocean world, the only one in the Solar System sustaining liquid surface water. Almost all of Earth's water is contained in its global ocean, covering 70.8% of Earth's crust. The remaining 29.2% of Earth's crust is land, most of which is located in the form of continental landmasses within Earth's land hemisphere. Most of Earth's land is at least somewhat humid and covered by vegetation, while large sheets of ice at Earth's polar deserts retain more water than Earth's groundwater, lakes, rivers, and atmospheric water combined. Earth's crust consists of slowly moving tectonic plates, which interact to produce mountain ranges, volcanoes, and earthquakes. Earth has a liquid outer core that generates a magnetosphere capable of deflecting most of the destructive solar winds and cosmic radiation. </p>
+        <p>Earth has a dynamic atmosphere, which sustains Earth's surface conditions and protects it from most meteoroids and UV-light at entry. It has a composition of primarily nitrogen and oxygen. Water vapor is widely present in the atmosphere, forming clouds that cover most of the planet. The water vapor acts as a greenhouse gas and, together with other greenhouse gases in the atmosphere, particularly carbon dioxide (CO2), creates the conditions for both liquid surface water and water vapor to persist via the capturing of energy from the Sun's light. This process maintains the current average surface temperature of 14.76 °C (58.57 °F), at which water is liquid under normal atmospheric pressure. Differences in the amount of captured energy between geographic regions (as with the equatorial region receiving more sunlight than the polar regions) drive atmospheric and ocean currents, producing a global climate system with different climate regions, and a range of weather phenomena such as precipitation, allowing components such as nitrogen to cycle. </p>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <h1>Hold to drag</h1>
+      <h1>Scroll to zoom</h1>
+    </header>
+  )
+}
+
+export default Header
